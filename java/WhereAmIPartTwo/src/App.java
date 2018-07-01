ec5632cdc7123cd72bbda97378e061a5b0590bd5
Zhiwei Fang <zfang@veracode.com>
2018-06-30 23:38:11 -0400

    location api

---
+++ java/WhereAmIPartTwo/src/App.java
@@ -0,0 +1,57 @@
+import org.json.*;
+import java.io.*;
+import java.net.*;
+
+public class App {
+	
+	public static void main(String[] args) throws JSONException {
+		String location = "";
+		JSONObject obj = null;
+		String maplink = "";
+		
+		// test case
+		location = getData("");
+		obj = new JSONObject(location);
+		System.out.println("\n\nYou are in or near the city of " 
+				+ obj.getString("city") + ", " + obj.getString("country"));
+		maplink = "https://www.google.com/maps/?q=" + obj.getString("loc");
+		System.out.println("Your approximate location on the map: \n" + maplink);
+		
+		location = getData("8.8.8.8");
+		obj = new JSONObject(location);
+		System.out.println("\n\nYou are in or near the city of "
+				+  obj.getString("city") + ", " + obj.getString("country"));
+		maplink = "https://www.google.com/maps/?q=" + obj.getString("loc");
+		System.out.println("You approximate location on the map: " + maplink);
+		
+	}
+	
+	public static String getData(String ip) {
+		URL url;
+		String response = "";
+		if (!ip.equals("")) {
+			ip = "/" + ip;
+		}
+		try {
+			// get URL content
+			String a = "https://ipinfo.io" + ip + "/json";
+			url = new URL(a);
+			URLConnection conn = url.openConnection();
+			
+			// open the stream and put it into BufferedReader
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			while ((inputLine = br.readLine()) != null) {
+				response = response + inputLine;
+			}
+			br.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return "";
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+		return response;
+	}
+}
