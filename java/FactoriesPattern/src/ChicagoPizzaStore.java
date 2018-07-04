dbaf1c5a76cf7bda43b156504da9511ef3dacb76
Zhiwei Fang <zfang@veracode.com>
2018-07-04 16:07:31 -0400

    factory

---
+++ java/FactoriesPattern/src/ChicagoPizzaStore.java
@@ -0,0 +1,11 @@
+
+public class ChicagoPizzaStore extends PizzaStore{
+	
+	Pizza createPizza(String item) {
+		if (item.equals("cheese")) {
+			return new ChicagoStyleCheesePizza();
+		} else {
+			return null;
+		}
+	}
+}
---
+++ java/FactoriesPattern/src/PizzaTestDrive.java
@@ -0,0 +1,17 @@
+
+public class PizzaTestDrive {
+	
+	public static void main(String[] args) {
+		PizzaStore nyStore = new NYPizzaStore();
+		PizzaStore chicagoStore = new ChicagoPizzaStore();
+ 
+		Pizza pizza = nyStore.orderPizza("cheese");
+		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+		
+		System.out.println(" - - - - - - - - - - - - - - - - ");
+		
+		pizza = chicagoStore.orderPizza("cheese");
+		System.out.println("someone ordered a " + pizza.getName() + "\n");
+		
+	}
+}
