a7fa0309d19788922e0e624d81710ebe40d58026
Zhiwei Fang <zfang@veracode.com>
2018-07-06 15:30:14 -0400

    writing

---
+++ java/ReadingAndWritingByteStreams/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		try (
+				FileInputStream in = new FileInputStream("testfile.txt");
+				FileOutputStream out = new FileOutputStream("new.txt");
+				) {
+			int c;
+			while ((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ java/ReadingAndWritingCharacterStreams/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		try(
+				FileReader in = new FileReader("testfile.txt");
+				FileWriter out = new FileWriter("newfile.txt");
+				){
+			int c;
+			while((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
